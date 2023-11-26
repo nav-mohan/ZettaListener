6166e48e828cc716c5da82e8a0d3521faa016e14
nav-mohan <navaneeth.mohan94@gmail.com>
2023-11-25 20:26:50 -0500

    template server and session on xmlparser

--- libs/server/server.hpp
+++ libs/server/server.hpp
@@ -10,13 +10,33 @@
 #include "session.hpp"
 #include "ms_logger.hpp"
 
+template <class XmlParser>
 class Server
 {
 public:
-    Server(boost::asio::io_context &io_context, short port);
-private:
-    void do_accept();
-    boost::asio::ip::tcp::acceptor acceptor_;
-};
+Server(boost::asio::io_context &io_context, short port)
+    : acceptor_(io_context, boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), port))
+{
+    basic_log("Server::Server",DEBUG);
+    do_accept();
+}
 
+private:
+void do_accept()
+{
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, boost::asio::ip::tcp::socket socket)
+        {
+            if (!ec)
+            {
+                basic_log("making shared connection",DEBUG);
+                std::make_shared<Session<XmlParser>>(std::move(socket))->start();
+            }
+
+            do_accept();
+        });
+}
+boost::asio::ip::tcp::acceptor acceptor_;
+
+};
 #endif // SERVER_HPP
--- libs/server/session.hpp
+++ libs/server/session.hpp
@@ -1,27 +1,65 @@
 #if !defined(SESION_HPP)
 #define SESION_HPP
-
 #include <boost/asio.hpp>
 #include <memory>
-
 #include "ms_logger.hpp"
 
-class Session : public std::enable_shared_from_this<Session>
+#define MAX_BUFFER_LEN 1024
+
+template<class XmlParser>
+class Session : public std::enable_shared_from_this<Session<XmlParser>>
 {
-public:
-    Session(boost::asio::ip::tcp::socket socket);
-    ~Session();
-    void start();
-
 private:
-    void do_read();
-    void do_write(std::size_t length);
     boost::asio::ip::tcp::socket socket_;
-    enum
-    {
-        max_length = 16
-    };
-    char data_[max_length];
-};
+    char data_[16];
+    XmlParser xmlparser_;
 
+void do_read()
+{
+    auto self(this->shared_from_this());
+    basic_log("Sesison::do_read " + std::to_string(self.use_count()));
+    socket_.async_read_some(boost::asio::buffer(data_, MAX_BUFFER_LEN),
+                            [this, self](boost::system::error_code ec, std::size_t length)
+                            {
+                                basic_log("READ LENGTH " + std::to_string(length));
+
+                                if (!ec)
+                                {
+                                    xmlparser_.parseXml(data_,length);
+                                    do_write(length);
+                                }
+                            });
+}
+
+void do_write(std::size_t length)
+{
+    auto self(this->shared_from_this());
+    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+                                [this, self](boost::system::error_code ec, std::size_t len)
+                                {
+                                    if (!ec)
+                                    {
+                                        do_read();
+                                    }
+                                });
+}
+
+public:
+Session(boost::asio::ip::tcp::socket socket) : socket_(std::move(socket))
+{
+    basic_log("Sesison::Session",DEBUG);
+}
+
+~Session()
+{
+    basic_log("Sesison::~Session",DEBUG);
+}
+
+void start()
+{
+    do_read();
+}
+
+
+};
 #endif // SESION_HPP
