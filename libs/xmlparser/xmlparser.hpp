631774f3e31832fc73307d7ed8318e385615c81a
nav-mohan <navaneeth.mohan94@gmail.com>
2023-12-31 12:37:46 -0500

    use unordered_map for storing result

--- libs/xmlparser/xmlparser.hpp
+++ libs/xmlparser/xmlparser.hpp
@@ -11,6 +11,7 @@
 #include <atomic>
 #include <queue>
 #include <vector>
+#include <unordered_map>
 
 #include <boost/regex.hpp> // boost::regex is a better library than std::regex, especially for multiline
 #include <boost/property_tree/ptree.hpp>
@@ -20,6 +21,37 @@
 #include "ms_logger.hpp"
 
 #define REGEX_PATTERN "<\\?xml\\s.*?<\\/ZettaClipboard>"
+
+const std::unordered_map<std::string,std::string> 
+DEFAULT_RESULT = {
+    {"LogEventID",      ""},
+    {"AirDate",         ""},
+    {"AirStartTime",    ""},
+    {"AirStopTime",     ""},
+
+    {"LogType",         ""},
+    {"ShowType",        ""},
+
+    {"Artist",          ""},
+    {"Album",           ""},
+    {"Title",           ""},
+
+// Asset Specific Metadata
+    {"AssetID",         ""},
+    {"AssetType",       ""},
+    {"AssetDuration",   ""},
+    {"AssetProduct",    ""},
+    {"AssetSponsor",    ""},
+    {"AssetFilePath",   ""},
+
+// Custom Attributes
+    {"RWReleaseDate",     ""},
+    {"RWLocal",           ""},
+    {"RWCanCon",          ""},
+    {"RWHit",             ""},
+    {"RWExplicit",        ""},
+    {"RWGenre",        ""}
+};
 
 struct ZettaFullXmlParser
 {
@@ -33,6 +65,15 @@ private:
     std::string buffer_;            
     std::queue<std::string> queue_; // a queue of regex-matching strings
     boost::property_tree::ptree ptree_;
+    std::unordered_map<std::string,std::string> result_ = DEFAULT_RESULT;
+    inline void resetResult(){result_ = DEFAULT_RESULT;}
+    inline void printResult()
+    {
+        std::string serializedOutput = "\n";
+        for(const auto [key,value]:  result_) 
+            serializedOutput += key + "--->" + value + "|\n";
+        basic_log(serializedOutput,DEBUG);
+    }
 
 public:
     void appendData(std::vector<char> &data, std::size_t dataSize); // to push strings into buffer_/queue_
