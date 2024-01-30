dcd8882d2f0b78542e90e5aec3ba423644b712fd
nav-mohan <navaneeth.mohan94@gmail.com>
2024-01-29 20:24:43 -0500

    call library code in client-code

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 
 ## Third-Pary Libraries 
 * `Boost::ASIO`
-* `Boost::Threads`
+* `Boost::Regex`
+* `Boost::PropertyTree`
+* `sqlite3`
 
 ## Custom Libraries
 * `ms_logger` for thread-safe logging
 * `server` for client-server TCP connection
+
+
+## Work in progress
+* `ZettaFullXmlParser` accepts custom attributes (`RW Release Date`,`Local`,`Cancon`,etc.)
+* `DbCon` accepts custom names for `MainTable` and `AttributeTable`.
+* `DbCon` accepts custom schema for `MainTable` and `AttributeTable`.
+* Save configurations (`ZettaFullXML/ZettaCleanXML`,`PortNumber`, `MainTableName`,`AttributeTableName`, `AutoConnect`) to a separate table `ConfigurationTable`.
--- main.cpp
+++ main.cpp
@@ -1,27 +1,48 @@
 #include "ms_logger.hpp"
-#include "server.hpp"
+#include "tcpserver.hpp"
 #include "xmlparser.hpp"
+#include "dbcon.hpp"
+#include "webapi.hpp"
+#include <functional>
 
 int main(int argc, char *argv[])
 {
-    try
+    if (argc != 3)
     {
-        if (argc != 2)
-        {
-            printf("USAGE: %s <PORT>\n",argv[0]);
-            return 1;
-        }
-
-        boost::asio::io_context io_context;
-
-        Server<ZettaFullXmlParser> s(io_context, std::atoi(argv[1]));
-
-        io_context.run();
+        printf("USAGE:   %s <TCP-PORT> <HTTP-PORT>\n",argv[0]);
+        printf("EXAMPLE: %s 10001 8001\n",argv[0]);
+        return 1;
     }
-    catch (std::exception &e)
+    int tcpPort = atoi(argv[1]);
+    int httpPort = atoi(argv[2]);
+
+    sqlite3 *db;
+    int retval = sqlite3_open("zettalogger.db", &db);
+    if (retval != SQLITE_OK)
     {
-        basic_log("Exception: " + std::string(e.what()));
+        fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
+        sqlite3_close(db);
+        return EXIT_FAILURE;
     }
+
+    DbCon dbc(db);
+
+    boost::asio::io_context io_context;
+
+    WebApi<DbCon> webapi(io_context, "0.0.0.0",httpPort, db); // i dont want the WebAPI making virtual calls
+    
+    auto writeToDatabase = [&dbc](std::unordered_map<std::string,std::string> record)
+    {
+        dbc.insertRecord(record);
+        auto latestRecord = dbc.getLatest();
+        // for(const auto [key,val] : latestRecord)
+        // {
+            // printf("LATEST %s --> %s\n",key.c_str(),val.c_str());
+        // }
+    };
+    TCPServer<ZettaFullXmlParser> s(io_context, tcpPort, writeToDatabase);
+
+    io_context.run();
 
     return 0;
 }
