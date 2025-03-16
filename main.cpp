437c510d5a63e8b92ea936fcd7a13134392df623
nav-mohan <navaneeth.mohan94@gmail.com>
2025-03-15 22:39:29 -0400

    Boilerplate code for GUI
    Building a basic GUI using ImGUI, GLFW, GLAD

--- main.cpp
+++ main.cpp
@@ -5,8 +5,55 @@
 #include "webapi.hpp"
 #include <functional>
 
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <backends/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_glfw.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+const char* glsl_version = "#version 330";
+void SetupGLFW() 
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+}
+
+void Cleanup(GLFWwindow* window) 
+{
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void SetupImGuiStyleAndFont() {
+    ImGuiIO& io = ImGui::GetIO();
+    
+    // Load a larger font (e.g., 20px)
+    if(io.Fonts->AddFontFromFileTTF("../fonts/ITCAvantGardeStd-Bk.otf", 20.0f))
+        ImGui_ImplOpenGL3_CreateFontsTexture();
+}
+
 int main(int argc, char *argv[])
 {
+    SetupGLFW();
+    GLFWwindow * window = glfwCreateWindow(800,600, "ZettaListener", nullptr,nullptr);
+    if(window == NULL) {printf("Failed to initialize window\n"); glfwTerminate();}
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    gladLoadGL();
+
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    SetupImGuiStyleAndFont();
+
     if (argc != 3)
     {
         printf("USAGE:   %s <TCP-PORT> <HTTP-PORT>\n",argv[0]);
@@ -43,6 +90,22 @@ int main(int argc, char *argv[])
     TCPServer<ZettaFullXmlParser> s(io_context, tcpPort, writeToDatabase);
 
     io_context.run();
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0,0,display_w,display_h);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+    Cleanup(window);
 
     return 0;
 }
